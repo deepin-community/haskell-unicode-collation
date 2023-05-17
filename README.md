45026b07ed6c9d5771f431a1f3e66ba3fb4e3f16
Yutao Meng <mengyutao@deepin.org>
2023-05-17 16:35:34 +0800

    chore: Init repo
    Log: Init repo
    Signed-off-by: Yutao Meng <mengyutao@deepin.org>

--- README.md
+++ README.md
@@ -1,1 +1,219 @@
-# template-repository
+# unicode-collation
+
+[![GitHub
+CI](https://github.com/jgm/unicode-collation/workflows/CI%20tests/badge.svg)](https://github.com/jgm/unicode-collation/actions)
+[![Hackage](https://img.shields.io/hackage/v/unicode-collation.svg?logo=haskell)](https://hackage.haskell.org/package/unicode-collation)
+[![BSD-2-Clause license](https://img.shields.io/badge/license-BSD--2--Clause-blue.svg)](LICENSE)
+
+Haskell implementation of [unicode collation algorithm].
+
+[unicode collation algorithm]:  https://www.unicode.org/reports/tr10
+
+## Motivation
+
+Previously there was no way to do correct unicode collation
+(sorting) in Haskell without depending on the C library `icu`
+and the barely maintained Haskell wrapper `text-icu`.  This
+library offers a pure Haskell solution.
+
+## Conformance
+
+The library passes all UCA conformance tests.
+
+Localized collations have not been tested as extensively.
+
+## Performance
+
+As might be expected, this library is slower than `text-icu`,
+which wraps a heavily optimized C library.  How much slower
+depends quite a bit on the input.
+
+On a sample of ten thousand random Unicode strings, we get a
+factor of about 3:
+
+```
+  sort a list of 10000 random Texts (en):
+    5.9 ms ± 487 μs,  22 MB allocated, 899 KB copied
+  sort same list with text-icu (en):
+    2.1 ms ±  87 μs, 7.1 MB allocated, 148 KB copied
+```
+
+Performance is worse on a sample drawn from a smaller character
+set including predominantly composed accented letters, which mut
+be decomposed as part of the algorithm:
+
+```
+  sort a list of 10000 Texts (composed latin) (en):
+     12 ms ± 1.1 ms,  34 MB allocated, 910 KB copied
+  sort same list with text-icu (en):
+    2.3 ms ±  56 μs, 7.0 MB allocated, 146 KB copied
+```
+
+Much of the impact here comes from normalization (decomposition).
+If we use a pre-normalized sample and disable normalization
+in the collator, it's much faster:
+
+```
+  sort same list but pre-normalized (en-u-kk-false):
+    5.4 ms ± 168 μs,  19 MB allocated, 909 KB copied
+```
+
+On plain ASCII, we get a factor of 3 again:
+
+```
+  sort a list of 10000 ASCII Texts (en):
+    4.6 ms ± 405 μs,  17 MB allocated, 880 KB copied
+  sort same list with text-icu (en):
+    1.6 ms ± 114 μs, 6.2 MB allocated, 130 KB copied
+```
+
+Note that this library does incremental normalization,
+so when strings can mostly be distinguished on the basis
+of the first two characters, as in the first sample, the
+impact is much less.  On the other hand, performance is
+much slower on a sample of texts which differ only after
+the first 32 characters:
+
+```
+  sort a list of 10000 random Texts that agree in first 32 chars:
+    116 ms ± 8.6 ms, 430 MB allocated, 710 KB copied
+  sort same list with text-icu (en):
+    3.2 ms ± 251 μs, 8.8 MB allocated, 222 KB copied
+```
+
+However, in the special case where the texts are identical,
+the algorithm can be short-circuited entirely and sorting
+is very fast:
+
+```
+  sort a list of 10000 identical Texts (en):
+    877 μs ±  54 μs, 462 KB allocated, 9.7 KB copied
+```
+
+## Localized collations
+
+The following localized collations are available.
+For languages not listed here, the root collation is
+used.
+
+```
+af
+ar
+as
+az
+be
+bn
+ca
+cs
+cu
+cy
+da
+de-AT-u-co-phonebk
+de-u-co-phonebk
+dsb
+ee
+eo
+es
+es-u-co-trad
+et
+fa
+fi
+fi-u-co-phonebk
+fil
+fo
+fr-CA
+gu
+ha
+haw
+he
+hi
+hr
+hu
+hy
+ig
+is
+ja
+kk
+kl
+kn
+ko
+kok
+lkt
+ln
+lt
+lv
+mk
+ml
+mr
+mt
+nb
+nn
+nso
+om
+or
+pa
+pl
+ro
+sa
+se
+si
+si-u-co-dict
+sk
+sl
+sq
+sr
+sv
+sv-u-co-reformed
+ta
+te
+th
+tn
+to
+tr
+ug-Cyrl
+uk
+ur
+vi
+vo
+wae
+wo
+yo
+zh
+zh-u-co-big5han
+zh-u-co-gb2312
+zh-u-co-pinyin
+zh-u-co-stroke
+zh-u-co-zhuyin
+```
+
+Collation reordering (e.g. `[reorder Latn Kana Hani]`)
+is not suported
+
+## Data files
+
+Version 13.0.0 of the Unicode data is used:
+<http://www.unicode.org/Public/UCA/13.0.0/>
+
+Locale-specific tailorings are derived from the Perl
+module Unicode::Collate:
+https://cpan.metacpan.org/authors/id/S/SA/SADAHIRO/Unicode-Collate-1.29.tar.gz
+
+## Executable
+
+The package includes an executable component, `unicode-collate`,
+which may be used for testing and for collating in scripts.
+To build it, enable the `executable` flag.
+For usage instructions, `unicode-collate --help`.
+
+## References
+
+- Unicode Technical Standard #35:
+  Unicode Locale Data Markup Language (LDML):
+  <http://www.unicode.org/reports/tr35/>
+- Unicode Technical Standard #10:
+  Unicode Collation Algorithm:
+  <https://www.unicode.org/reports/tr10>
+- Unicode Technical Standard #215:
+  Unicode Normalization Forms:
+  <https://unicode.org/reports/tr15/>
+
---
+++ src/Text/Collate/Lang.hs
@@ -0,0 +1,221 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE DeriveLift #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Text.Collate.Lang
+  ( Lang(..)
+  , parseLang
+  , renderLang
+  , lookupLang
+  )
+where
+import Data.Maybe (listToMaybe, mapMaybe)
+import Control.Monad (mzero)
+import Data.Ord (Down(..))
+import Data.List (sortOn)
+import Data.Char (isAlphaNum, isAscii, isDigit, isSpace, isAlpha)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Text.Parsec as P
+import Data.Binary (Binary(..))
+import Data.String
+import Language.Haskell.TH.Syntax (Lift(..))
+import Instances.TH.Lift ()
+#if MIN_VERSION_base(4,11,0)
+#else
+import Data.Semigroup (Semigroup(..))
+#endif
+
+-- | Represents a BCP 47 language tag (<https://tools.ietf.org/html/bcp47>).
+data Lang = Lang{ langLanguage   :: Text
+                , langScript     :: Maybe Text
+                , langRegion     :: Maybe Text
+                , langVariants   :: [Text]
+                , langExtensions :: [(Text, [(Text , Text)])]
+                , langPrivateUse :: [Text]
+                } deriving (Eq, Ord, Show, Lift)
+
+instance IsString Lang where
+ fromString =
+   fromRight (Lang "und" Nothing Nothing [] [] []) . parseLang . T.pack
+
+instance Binary Lang where
+ put (Lang a b c d e f) = put (a,b,c,d,e,f)
+ get = do
+     (a,b,c,d,e,f) <- get
+     return $ Lang a b c d e f
+
+-- | Find best match for a 'Lang' in an association list.
+lookupLang :: Lang -> [(Lang, a)] -> Maybe (Lang, a)
+lookupLang lang =
+    fmap snd
+  . listToMaybe
+  . sortOn (Down . fst)
+  . mapMaybe (\(l,t) ->
+       case match l of
+         Nothing -> Nothing
+         Just x -> Just (x,(l,t)))
+ where
+  langsMatch l = if langLanguage lang == langLanguage l
+                    then Just True
+                    else Nothing
+  maybeMatch f l = case (f l, f lang) of
+                     (Nothing, Nothing) -> Just True
+                     (Nothing, Just _)  -> Just False
+                     (Just x, mby)      -> case mby of
+                                             Just y | x == y -> Just True
+                                             _ -> Nothing
+  langCollation l = lookup "u" (langExtensions l) >>= lookup "co"
+  match l = do
+    lm <- langsMatch l
+    sm <- maybeMatch langScript l
+    rm <- maybeMatch langRegion l
+    cm <- maybeMatch langCollation l
+    return (lm,sm,rm,cm)
+
+-- | Render a 'Lang' in BCP 47 form.
+renderLang :: Lang -> Text
+renderLang lang =
+    langLanguage lang
+     <> maybe "" (T.cons '-') (langScript lang)
+     <> maybe "" (T.cons '-') (langRegion lang)
+     <> mconcat (map (T.cons '-') (langVariants lang))
+     <> mconcat (map renderExtension (langExtensions lang))
+     <> renderPrivateUse (langPrivateUse lang)
+ where
+  renderExtension (c, ks) = "-" <> c <> mconcat (map renderKeyword ks)
+  renderKeyword (k, v) = "-" <> k <> if T.null v
+                                        then ""
+                                        else "-" <> v
+  renderPrivateUse [] = ""
+  renderPrivateUse ts = "-x" <> mconcat (map (T.cons '-') ts)
+
+-- | Parse a BCP 47 language tag as a 'Lang'.
+parseLang :: Text -> Either String Lang
+parseLang lang =
+  case P.parse pLangTag "lang" (T.split (\c -> c == '-' || c == '_')
+                                 $ T.takeWhile (not . isSpace) lang) of
+       Right r -> Right r
+       Left e  -> Left $ show e
+  where
+    -- langtag       = language
+    --                ["-" script]
+    --                ["-" region]
+    --                 *("-" variant)
+    --                 *("-" extension)
+    pLangTag = do
+      language <- pLanguage P.<?> "language"
+      script <- P.option Nothing (Just <$> pScript P.<?> "script")
+      region <- P.option Nothing (Just <$> pRegion P.<?> "region")
+      variants <- P.many pVariant P.<?> "variant"
+      extensions <- P.many pExtension P.<?> "extension"
+      privateUse <- P.option [] (pPrivateUse P.<?> "private use")
+      -- P.eof  -- like CSL, we allow garbage afterwards
+      return Lang{   langLanguage = language
+                   , langScript = script
+                   , langRegion = region
+                   , langVariants = variants
+                   , langExtensions = extensions
+                   , langPrivateUse = privateUse }
+
+    -- language      = 2*3ALPHA            ; shortest ISO 639 code
+    --                 ["-" extlang]       ; sometimes followed by
+    --                                     ; extended language subtags
+    --               / 4ALPHA              ; or reserved for future use
+    --               / 5*8ALPHA            ; or registered language subtag
+    pLanguage = (do
+      baselang <- T.toLower <$> alphasBetween 2 3
+      extlang <- P.option Nothing $ Just <$> pExtlang
+      case extlang of
+        Nothing  -> pure baselang
+        Just ext -> pure $ baselang <> "-" <> ext)
+      P.<|> T.toLower <$> alphasBetween 4 8
+
+    -- extlang       = 3ALPHA              ; selected ISO 639 codes
+    --                 *2("-" 3ALPHA)      ; permanently reserved
+    pExtlang = T.intercalate "-" <$> countBetween 1 3
+                 (T.toLower <$> alphas 3)
+
+    -- script        = 4ALPHA              ; ISO 15924 code
+    pScript = T.toTitle <$> alphas 4
+
+    -- region        = 2ALPHA              ; ISO 3166-1 code
+    --               / 3DIGIT              ; UN M.49 code
+    pRegion = T.toUpper <$> alphas 2 P.<|> digits 3
+
+    -- variant       = 5*8alphanum         ; registered variants
+    --              / (DIGIT 3alphanum)
+    pVariant = T.toLower <$>
+      (alphanumsBetween 5 8
+       P.<|> tok (\t -> T.all isAsciiAlphaNum t && T.length t == 4 &&
+                                    isDigit (T.head t)))
+
+    -- extension     = singleton 1*("-" (2*8alphanum))
+    -- RFC6087:
+    -- An 'attribute' is a subtag with a length of three to eight
+    -- characters following the singleton and preceding any 'keyword'
+    -- sequences.  No attributes were defined at the time of this
+    -- document's publication.
+
+    -- A 'keyword' is a sequence of subtags consisting of a 'key' subtag,
+    -- followed by zero or more 'type' subtags (so a 'key' might appear
+    -- alone and not be accompanied by a 'type' subtag).  A 'key' MUST
+    -- NOT appear more than once in a language tag's extension string.
+    -- The order of the 'type' subtags within a 'keyword' is sometimes
+    -- significant to their interpretation.
+
+    -- A.  A 'key' is a subtag with a length of exactly two characters.
+    --     Each 'key' is followed by zero or more 'type' subtags.
+
+    -- B.  A 'type' is a subtag with a length of three to eight
+    --     characters following a 'key'.  'Type' subtags are specific to
+    --     a particular 'key' and the order of the 'type' subtags MAY be
+    --     significant to the interpretation of the 'keyword'.
+    pExtension = do
+      c <- T.toLower <$> tok (\t -> T.length t == 1 && T.all isAsciiAlphaNum t)
+      attrs <- P.many
+             (T.toLower <$>
+               tok (\t -> T.all isAsciiAlphaNum t && lengthBetween 3 8 t))
+      keywords <- P.many pKeyword
+      return (c, map (, "") attrs ++ keywords)
+
+    pKeyword = do
+      key <- alphas 2
+      types <- P.many (alphanumsBetween 3 8)
+      return (key, T.intercalate "-" types)
+
+    -- privateuse    = "x" 1*("-" (1*8alphanum))
+    pPrivateUse = do
+      _ <- tok (\t -> T.toLower t == "x")
+      P.many1 (alphanumsBetween 1 8)
+
+    tok :: (Text -> Bool) -> P.Parsec [Text] () Text
+    tok f = P.tokenPrim T.unpack (\pos t _ ->
+                                   P.incSourceColumn pos (T.length t))
+                      (\t -> if f t then Just t else Nothing)
+
+    countBetween (low :: Int) (hi :: Int) p = P.try $ countBetween' low hi p 1
+    countBetween' low hi p (n :: Int) = (do
+     res <- p
+     if n >= hi
+        then return [res]
+        else (res:) <$> countBetween' low hi p (n + 1))
+      P.<|> (if n > low then return [] else mzero)
+
+    isAsciiAlpha c = isAscii c && isAlpha c
+    alphas len = tok (\t -> T.all isAsciiAlpha t && T.length t == len)
+    digits len = tok (\t -> T.all isDigit t && T.length t == len)
+    alphasBetween minLen maxLen =
+      tok (\t -> T.all isAsciiAlpha t && lengthBetween minLen maxLen t)
+    alphanumsBetween minLen maxLen =
+      tok (\t -> T.all isAsciiAlphaNum t && lengthBetween minLen maxLen t)
+    lengthBetween lo hi t = let len = T.length t in len >= lo && len <= hi
+    isAsciiAlphaNum c = isAscii c && isAlphaNum c
+
+
+-- We define fromRight here instead of importing it,
+-- because it doesn't exist in some base versions we support.
+fromRight :: b -> Either a b -> b
+fromRight fallback (Left _)  = fallback
+fromRight _        (Right x) = x
